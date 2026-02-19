8c970a61d113addef59e252043fef0a258f72faf
basbaer <bastianbaer@hotmail.com>
2026-02-19 14:03:48 +0100

    feat: Add db migration for activation code

---
+++ src/database/migrations/2026_02_19_125607_add_activation_code.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Add activation_code column to users table
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('activation_code', 20)->nullable()->after('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Remove activation_code column from users table
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('activation_code');
+        });
+    }
+};
