bffacd684ddf5505287bdc03da2d49cd20fca9d4
Bas <bastianbaer@hotmail.com>
2026-04-16 14:40:44 +0200

    feat: Create table for forstwirt logging

---
+++ src/database/seeders/ForstwirtWorkingTypeSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ForstwirtWorkingTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $slugs = ['motorsage', 'freischneider', 'seilmaschine', 'messkluppe', 'reparatur', 'other'];
+        $types = ['Motorsäge', 'Freischneider', 'Seilmaschine', 'Messkluppe', 'Reparatur', 'Sonstiges'];
+
+        foreach ($types as $index => $type) {
+            DB::table('forstwirt_working_types')->upsert(
+                [
+                    'id' => $index,
+                    'slug' => $slugs[$index],
+                    'name' => $type,
+                ],
+                ['slug'], // Unique constraint for upsert
+                ['name']  // Fields to update if the record already exists
+            );
+        }
+    }
+}
