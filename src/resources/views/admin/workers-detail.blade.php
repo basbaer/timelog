71e400b7b791eecf2af1c42fc29ce403fde35c0e
Bas <bastianbaer@hotmail.com>
2026-06-26 12:34:31 +0200

    refactor: Make partial for top section of worker-detail

--- src/resources/views/admin/workers-detail.blade.php
+++ src/resources/views/admin/workers-detail.blade.php
@@ -6,38 +6,13 @@
 <body>
     @include('partials.admin_navbar', ['active' => 'workers'])
 
-    <div class="mt-3">
-        <div class="row">
-            <div class="col justify-content-start d-flex">
-                <h2 class="h2 m-3">{{ $name }}</h2>
-            </div>
-            <div class="col justify-content-end d-flex">
-                <a href="{{ route('admin.worker.card', ['worker_id' => $worker_id]) }}" type="button"
-                    class="btn btn-secondary m-3">Kontaktinformationen</a>
-            </div>
-        </div>
-    </div>
-
-    @if (session('success'))
-        <div class="container mt-3">
-
-            <div class="alert alert-success" role="alert">
-                {{ session('success') }}
-            </div>
-        </div>
-    @endif
-
-    <div class="container mt-3 d-flex justify-content-center align-items-center gap-4">
-        <a href="{{ route('admin.worker.show', ['worker_id' => $worker_id, 'month' => $previousMonth]) }}"
-            id="previousMonthBtn" aria-label="Vorheriger Monat">
-            <i class="bi bi-caret-left-fill" style="font-size: 1.5rem; color: grey;" aria-hidden="true"></i>
-        </a>
-        <h3 class="mb-0">{{ $month }}</h3>
-        <a href="{{ route('admin.worker.show', ['worker_id' => $worker_id, 'month' => $nextMonth]) }}" 
-            id="nextMonthBtn" aria-label="Nächster Monat">
-            <i class="bi bi-caret-right-fill" style="font-size: 1.5rem; color: grey;" aria-hidden="true"></i>
-        </a>
-    </div>
+    @include('partials.admin_worker_detail_top', [
+        'name' => $name,
+        'worker_id' => $worker_id,
+        'month' => $month,
+        'previousMonth' => $previousMonth,
+        'nextMonth' => $nextMonth,
+    ])
 
     <div class=" m-3">
         <table class="table">
