241cb853ea5cb1c30c625e8f65ae235c02ec233e
Bas <bastianbaer@hotmail.com>
2026-06-30 10:09:22 +0200

    feat: Add 'Übersicht Mitarbeiter' to worker overview

--- src/resources/views/admin/workers-overview.blade.php
+++ src/resources/views/admin/workers-overview.blade.php
@@ -6,12 +6,14 @@
 <body>
     @include('partials.admin_navbar', ['active' => 'workers'])
 
-    <div class="container mt-3">
+    <div class="container my-3">
+        <h2 class="mt-3">Übersicht Mitarbeiter</h2>
         <div class="list-group">
-           @foreach ($workers as $worker)
-               <a href="{{ route('admin.worker.show', $worker->id) }}" class="list-group-item list-group-item-action">{{ $worker->first_name . " " . $worker->last_name }}</a>
-           @endforeach
-            
+            @foreach ($workers as $worker)
+                <a href="{{ route('admin.worker.show', $worker->id) }}"
+                    class="list-group-item list-group-item-action">{{ $worker->first_name . ' ' . $worker->last_name }}</a>
+            @endforeach
+
         </div>
     </div>
 
@@ -20,8 +22,8 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
 
 </body>
 
