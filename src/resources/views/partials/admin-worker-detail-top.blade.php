1562ca096848029e02102c4a0e8094436b3de72a
Bas <bastianbaer@hotmail.com>
2026-07-15 20:24:44 +0200

    feat: Move month selection in row above

--- src/resources/views/partials/admin-worker-detail-top.blade.php
+++ src/resources/views/partials/admin-worker-detail-top.blade.php
@@ -1,33 +1,38 @@
  <div class="mt-3">
-        <div class="row">
-            <div class="col justify-content-start d-flex">
-                <h2 class="h2 m-3">{{ $name }}</h2>
-            </div>
-            <div class="col justify-content-end d-flex">
-                <a href="{{ route('admin.worker.card', ['worker_id' => $worker_id]) }}" type="button"
-                    class="btn btn-secondary m-3">Kontaktinformationen</a>
-            </div>
-        </div>
-    </div>
 
-    @if (session('success'))
-        <div class="container mt-3">
+     @if (session('success'))
+         <div class="container mt-3">
 
-            <div class="alert alert-success" role="alert">
-                {{ session('success') }}
-            </div>
-        </div>
-    @endif
+             <div class="alert alert-success" role="alert">
+                 {{ session('success') }}
+             </div>
+         </div>
+     @endif
 
-    <div class="container mt-3 d-flex justify-content-center align-items-center gap-4">
-        <a href="{{ route('admin.worker.show', ['worker_id' => $worker_id, 'month' => $previousMonth]) }}"
-            id="previousMonthBtn" aria-label="Vorheriger Monat">
-            <i class="bi bi-caret-left-fill" style="font-size: 1.5rem; color: grey;" aria-hidden="true"></i>
-        </a>
-        <h3 class="mb-0">{{ $month }}</h3>
-        <a href="{{ route('admin.worker.show', ['worker_id' => $worker_id, 'month' => $nextMonth]) }}" 
-            id="nextMonthBtn" aria-label="Nächster Monat">
-            <i class="bi bi-caret-right-fill" style="font-size: 1.5rem; color: grey;" aria-hidden="true"></i>
-        </a>
-    </div>
-</div>
+     <div class="row">
+         <div class="col justify-content-start d-flex">
+             <h2 class="h2 m-3">{{ $name }}</h2>
+         </div>
+
+         <div class="col mt-3 d-flex justify-content-center align-items-center gap-4">
+             <a href="{{ route('admin.worker.show', ['worker_id' => $worker_id, 'month' => $previousMonth]) }}"
+                 id="previousMonthBtn" aria-label="Vorheriger Monat">
+                 <i class="bi bi-caret-left-fill" style="font-size: 1.5rem; color: grey;" aria-hidden="true"></i>
+             </a>
+             <h3 class="mb-0">{{ $month }}</h3>
+             <a href="{{ route('admin.worker.show', ['worker_id' => $worker_id, 'month' => $nextMonth]) }}"
+                 id="nextMonthBtn" aria-label="Nächster Monat">
+                 <i class="bi bi-caret-right-fill" style="font-size: 1.5rem; color: grey;" aria-hidden="true"></i>
+             </a>
+         </div>
+
+        
+         <div class="col justify-content-end d-flex">
+             <a href="{{ route('admin.worker.card', ['worker_id' => $worker_id]) }}" type="button"
+                 class="btn btn-secondary m-3">Kontaktinformationen</a>
+         </div>
+     </div>
+ </div>
+
+
+ </div>
